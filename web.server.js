7254b6c10ac2f1bcb0e62029fa5bc50c77ebd538
Austin Thiel <thiel105@mail.chapman.edu>
2016-10-22 14:54:03 -0700

    initial commit

---
+++ web.server.js
@@ -0,0 +1,13 @@
+/* jshint node: true */
+'use strict';
+
+let express = require("express");
+let app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function() {
+  console.log("Listening on port 3000");
+});
